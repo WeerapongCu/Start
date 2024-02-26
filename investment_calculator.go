dea350354cc13f05357713c83364ed66721ea5e5
= <=>
2024-02-26 15:05:07 +0700

    add text on output

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,14 +8,21 @@ import (
 func main() {
 	const intflationRate = 2.5
 	var investmentAmount float64
-	years := 10.0
-	expactedReturnRate := 5.5
+	var years float64
+	var expactedReturnRate float64
 
+	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expactedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1 + expactedReturnRate / 100, years)
 	futureRealValue := futureValue / math.Pow(1+intflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	fmt.Println("Future Value:", futureValue)
+	fmt.Println("Future Value (adjusted for Inflation)", futureRealValue)
 }
